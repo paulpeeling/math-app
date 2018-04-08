efea1aede93dd29b4a9ffeb3ea3ad70287b2c03a
Paul Peeling <ppeeling@mathworks.com>
2018-04-08 23:22:44 +0100

    built addition module

--- src/app/problem/problem.component.ts
+++ src/app/problem/problem.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Sum } from '../sum';
 
 @Component({
   selector: 'app-problem',
   templateUrl: './problem.component.html',
   styleUrls: ['./problem.component.css']
 })
+
 export class ProblemComponent implements OnInit {
 
-  constructor() { }
+  answerDigit1 : number = 0;
+  answerDigit2 : number = 0;
+  answerDigit3 : number = 0;
+  answerDigit4 : number = 0;
+
+  problem: Sum;
+
+  correct: boolean = false;
+
+  constructor() { 
+    this.problem = new Sum();
+  }
 
   ngOnInit() {
   }
 
+  onCheck() {
+    var answer = this.answerDigit1*1000 + this.answerDigit2*100 + this.answerDigit3*10 + this.answerDigit4*1; 
+    if (answer == this.problem.first + this.problem.second)
+    {      
+      this.correct = true;
+    }
+  }
+
+  onNew() {
+    this.correct = false;
+    this.problem = new Sum();
+  }
 }
---
+++ src/app/sum.ts
@@ -0,0 +1,8 @@
+export class Sum {
+    first: number;
+    second: number;  
+    constructor() {
+      this.first = Math.round(Math.random()*999);
+      this.second = Math.round(Math.random()*999);
+    }
+  }
