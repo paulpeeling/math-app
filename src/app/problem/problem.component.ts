821536581e5521016093292ca0b6f86d0afbcd62
Paul Peeling <ppeeling@mathworks.com>
2018-04-09 22:55:27 +0100

    easier/harder controls for problem size. resolves #2

--- src/app/problem/problem.component.ts
+++ src/app/problem/problem.component.ts
@@ -13,21 +13,29 @@ export class ProblemComponent implements OnInit {
   answerDigit2 : number = 0;
   answerDigit3 : number = 0;
   answerDigit4 : number = 0;
+  answerDigit5 : number = 0;
 
   problem: Sum;
+
+  upperSize : number = 3;
+  lowerSize : number = 3;
 
   correct: boolean = false;
   operation: string;
 
   constructor() { 
-    this.problem = new Sum("+");
+    this.problem = new Sum("+", this.upperSize, this.lowerSize);
   }
 
   ngOnInit() {
   }
 
   onCheck() {
-    var answer = this.answerDigit1*1000 + this.answerDigit2*100 + this.answerDigit3*10 + this.answerDigit4*1; 
+    var answer = this.answerDigit1*10000 + 
+      this.answerDigit2*1000 + 
+      this.answerDigit3*100 + 
+      this.answerDigit4*10 + 
+      this.answerDigit5*1; 
     var expected = this.problem.first + this.problem.second;
     if (this.operation == "-")
     {
@@ -41,13 +49,35 @@ export class ProblemComponent implements OnInit {
 
   onNew() {
     this.correct = false;
-    this.problem = new Sum(this.operation);
+    this.problem = new Sum("+", this.upperSize, this.lowerSize);
   }
 
   onChange(value:string) : void {
     this.operation = value;
     this.correct = false;
-    this.problem = new Sum(this.operation);
+    this.problem = new Sum("+", this.upperSize, this.lowerSize);
+  }
+
+  onEasier() {
+    if (this.upperSize == this.lowerSize)
+    {
+      this.lowerSize--;
+    } else {
+      this.upperSize--;
+    }
+    this.correct = false;
+    this.problem = new Sum("+", this.upperSize, this.lowerSize);
+  }
+
+  onHarder() {
+    if (this.upperSize == this.lowerSize) 
+    {
+      this.upperSize++;
+    } else {
+      this.lowerSize++;
+    }
+    this.correct = false;
+    this.problem = new Sum("+", this.upperSize, this.lowerSize);
   }
 
 }
