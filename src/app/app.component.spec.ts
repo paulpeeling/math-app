5077655110e38545f7fa7bd86e38586aa6eb9157
Paul Peeling <ppeeling@mathworks.com>
2018-04-10 07:55:05 +0100

    unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ProblemComponent } from './problem/problem.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ProblemComponent
       ],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +20,9 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Math-App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(app.title).toEqual('Math-App');
   }));
 });
--- src/app/problem/problem.component.spec.ts
+++ src/app/problem/problem.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement }    from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { ProblemComponent } from './problem.component';
+import { FormsModule } from '@angular/forms';
+
 
 describe('ProblemComponent', () => {
   let component: ProblemComponent;
@@ -8,7 +12,8 @@ describe('ProblemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProblemComponent ]
+      declarations: [ ProblemComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   }));
@@ -19,7 +24,32 @@ describe('ProblemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the problem component with initially incorrect answer', () => {
     expect(component).toBeTruthy();
+    expect(component.correct).toBeFalsy();
   });
+
+  it('should display four buttons', () => {
+    var buttons = fixture.debugElement.queryAll(By.css('button'));
+    expect(buttons.length).toEqual(4);
+  });
+
+  it('should set correct when the answer is correct', () => {
+    expect(component.correct).toBeFalsy();
+    component.problem.first = 1;
+    component.problem.second = 1;
+    component.answerDigit5 = 2;
+    component.onCheck();
+    expect(component.correct).toBeTruthy();
+  });
+
+  it('should set reset correct when changing operation', () => {
+    component.problem.first = 0;
+    component.problem.second = 0;
+    component.onCheck();
+    expect(component.correct).toBeTruthy();
+    component.onChange("-");
+    expect(component.correct).toBeFalsy();
+  });
+
 });
